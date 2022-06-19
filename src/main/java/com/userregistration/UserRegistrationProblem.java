baffbddb9bcdd6d85a16d7d0fe01b227411ce9aa
Charsh177 <harshchougule177@gmail.com>
2022-06-19 20:46:52 +0530

    ADDED: Welcome to user registration program

---
+++ src/main/java/com/userregistration/UserRegistrationProblem.java
@@ -0,0 +1,9 @@
+package com.userregistration;
+
+public class UserRegistrationProblem {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to User Registration Program");
+    }
+}
