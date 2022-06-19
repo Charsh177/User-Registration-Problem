99bd04379cda8a867cc017caea7660cfa01fd503
Charsh177 <harshchougule177@gmail.com>
2022-06-20 01:06:59 +0530

    ADDED: Should clear all email samples provided separately

--- src/main/java/com/userregistration/UserRegistrationProblem.java
+++ src/main/java/com/userregistration/UserRegistrationProblem.java
@@ -9,37 +9,37 @@ public class UserRegistrationProblem {
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
-//        System.out.println("Enter your First Name : ");
-//        String firstName= sc.nextLine();
-//        System.out.println(Pattern.matches("^[A-Z]{1}[a-z]{2,}",firstName));
-//
-//        System.out.println("Enter your Last Name : ");
-//        String lastName= sc.nextLine();
-//        System.out.println(Pattern.matches("^[A-Z]{1}[a-z]{2,}",lastName));
-//
-//        System.out.println("Enter your email Id : ");
-//        String emailId= sc.nextLine();
-//        System.out.println(Pattern.matches("^[0-9A-Za-z]+(([._+-]?)[0-9A-Za-z]+)*@[0-9A-Za-z]+.[a-z]{2,4}.([a-z]{2,3})*$", emailId));
-//
-//        System.out.println("Enter your Mobile Number : ");
-//        String mobileNumber= sc.nextLine();
-//        System.out.println(Pattern.matches("(91\\s)?[6-9][0-9]{9}$",mobileNumber));
-//
-//        System.out.println("Enter your Password : ");
-//        String password= sc.nextLine();
-//        System.out.println(Pattern.matches("[a-zA-Z]{8,}",password));
-//
-//        System.out.println("Enter your Password : ");
-//        String upperPassword= sc.nextLine();
-//        System.out.println(Pattern.matches("[A-Z]+[a-z]{8,}",upperPassword));
-//
-//        System.out.println("Enter your Password : ");
-//        String numPassword= sc.nextLine();
-//        System.out.println(Pattern.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",numPassword));
-//
-//        System.out.println("Enter your Password : ");
-//        String scharPassword= sc.nextLine();
-//        System.out.println(Pattern.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",scharPassword));
+        System.out.println("Enter your First Name : ");
+        String firstName= sc.nextLine();
+        System.out.println(Pattern.matches("^[A-Z]{1}[a-z]{2,}",firstName));
+
+        System.out.println("Enter your Last Name : ");
+        String lastName= sc.nextLine();
+        System.out.println(Pattern.matches("^[A-Z]{1}[a-z]{2,}",lastName));
+
+        System.out.println("Enter your email Id : ");
+        String emailId= sc.nextLine();
+        System.out.println(Pattern.matches("^[0-9A-Za-z]+(([._+-]?)[0-9A-Za-z]+)*@[0-9A-Za-z]+.[a-z]{2,4}.([a-z]{2,3})*$", emailId));
+
+        System.out.println("Enter your Mobile Number : ");
+        String mobileNumber= sc.nextLine();
+        System.out.println(Pattern.matches("(91\\s)?[6-9][0-9]{9}$",mobileNumber));
+
+        System.out.println("Enter your Password : ");
+        String password= sc.nextLine();
+        System.out.println(Pattern.matches("[a-zA-Z]{8,}",password));
+
+        System.out.println("Enter your Password : ");
+        String upperPassword= sc.nextLine();
+        System.out.println(Pattern.matches("[A-Z]+[a-z]{8,}",upperPassword));
+
+        System.out.println("Enter your Password : ");
+        String numPassword= sc.nextLine();
+        System.out.println(Pattern.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",numPassword));
+
+        System.out.println("Enter your Password : ");
+        String scharPassword= sc.nextLine();
+        System.out.println(Pattern.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",scharPassword));
 
         String[] validEmail={"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com",
                 "abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"};
